90bb64a861c483d442a8c77b2501b23e2142cbe7
Antonia Siu <mail@antoniasiu.co.uk>
2021-03-22 13:30:44 +0000

    only use the on_translated hook

--- plover_cards/card_suggestions.py
+++ plover_cards/card_suggestions.py
@@ -1,22 +1,7 @@
 from pathlib import Path
 import pickle
-import re
 
 from plover.oslayer.config import CONFIG_DIR as PLOVER_CONFIG_DIR
-from plover.translation import escape_translation
-from plover.orthography import add_suffix
-
-SUFFIX_PATTERN = re.compile(r"^(.+) {\^(.+)}$")
-
-
-def apply_suffix(text):
-    match = SUFFIX_PATTERN.match(text)
-
-    if match:
-        print("match", match)
-        return add_suffix(match.group(1), match.group(2))
-
-    return text
 
 
 class CardSuggestions:
@@ -35,9 +20,9 @@ class CardSuggestions:
         with self.PATH.open("wb") as f:
             pickle.dump(self.card_suggestions, f)
 
-    def add(self, text, stroke_suggestions):
-        if text is None:
-            return
+    def add_suggestion(self, suggestion):
+        text = suggestion.text
+        stroke_suggestions = ["/".join(s) for s in suggestion.steno_list]
 
         if self.card_suggestions.get(text) is None:
             self.card_suggestions[text] = {
@@ -47,18 +32,6 @@ class CardSuggestions:
 
         self.card_suggestions[text]["frequency"] += 1
         self.card_suggestions[text]["strokes"].update(stroke_suggestions)
-
-    def add_suggestion(self, suggestion):
-        stroke_suggestions = ["/".join(s) for s in suggestion.steno_list]
-        self.add(suggestion.text, stroke_suggestions)
-
-    def add_translation(self, translation):
-        if translation.english is None:
-            return
-
-        text = escape_translation(translation.english)
-        text = apply_suffix(text)
-        self.add(text, ["/".join(translation.rtfcre)])
 
     def delete(self, text):
         del self.card_suggestions[text]
--- plover_cards/plover_hook.py
+++ plover_cards/plover_hook.py
@@ -5,21 +5,9 @@ from plover.formatting import RetroFormatter
 from .card_suggestions import CardSuggestions
 
 
-def tails(ls):
-    """Return all tail combinations
-    tails :: [x] -> [[x]]
-    >>> tails('abcd')
-    ['abcd', 'bcd', 'cd', d']
-
-    https://github.com/openstenoproject/plover/blob/91a84e16403e9d7470d0192c3b5484e422060a0b/plover/gui_qt/suggestions_dialog.py#L106
-    """
-
-    for i in range(len(ls)):
-        yield ls[i:]
-
-
 class Main:
 
+    # from https://github.com/openstenoproject/plover/blob/91a84e16403e9d7470d0192c3b5484e422060a0b/plover/gui_qt/suggestions_dialog.py#L36
     WORD_RX = re.compile(r"(?:\w+|[^\w\s]+)\s*")
 
     def __init__(self, engine):
@@ -30,19 +18,14 @@ class Main:
 
     def start(self):
         self.engine.hook_connect("translated", self._on_translated)
-        self.engine._translator.add_listener(self._on_translator_output)
 
     def stop(self):
         self.engine.hook_disconnect("translated", self._on_translated)
         self.card_suggestions.save()
 
     def _on_translated(self, _old, new):
-        """https://github.com/openstenoproject/plover/blob/91a84e16403e9d7470d0192c3b5484e422060a0b/plover/gui_qt/suggestions_dialog.py#L118"""
-
-        for action in reversed(new):
-            if action.text and not action.text.isspace():
-                break
-        else:
+        if len(new) == 0:
+            # true if the stroke is an undo stroke
             return
 
         with self.engine:
@@ -50,11 +33,11 @@ class Main:
             retro_formatter = RetroFormatter(last_translations)
             split_words = retro_formatter.last_words(10, rx=self.WORD_RX)
 
-        for phrase in tails(split_words):
-            phrase = "".join(phrase)
+        # last few "phrases", e.g. "let's go", "'s go", "s go", "go"
+        phrases = set(("".join(split_words[i:]) for i in range(len(split_words))))
+        # last translation in case it isn't shown exactly, e.g. "{#Return}{^}", {^ing}
+        phrases.add(last_translations[-1].english)
+
+        for phrase in phrases:
             for suggestion in self.engine.get_suggestions(phrase):
                 self.card_suggestions.add_suggestion(suggestion)
-
-    def _on_translator_output(self, _undo, do, _prev):
-        for translation in do:
-            self.card_suggestions.add_translation(translation)
